ef26a5728dd8d3aa14887e689ec02f294d5a579d
clnzbqll <clnzbqll@tiedada.com>
2023-08-18 16:38:07 +0800

    # v-on测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-    <vFor/>
+    <vOn/>
 </template>
 
 <script setup>
-import vBind from './components/test/vBind.vue'
-import vIfElse from './components/test/vIfElse.vue'
-import vFor from './components/test/vFor.vue'
+import vBind from './components/test/instruct/vBind.vue'
+import vIfElse from './components/test/instruct/vIfElse.vue'
+import vFor from './components/test/instruct/vFor.vue'
+import vOn from './components/test/event/vOn.vue'
 </script>
---
+++ src/components/test/event/vOn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- v-on指令用户监听DOM事件，可简写为@ -->
+        <!-- v-on传参不用指明参数类型，用$event可以传递参数本身 -->
+        <button @click="login(msg, $event)">登录</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg: "登录成功"
+        }
+    },
+    methods:{
+        login(msg, e){
+            console.log(msg)
+            console.log(e)
+        }
+    }
+}
+</script>
