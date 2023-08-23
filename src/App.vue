7359126d7687b5046d48e7debaeb9ebf24275f87
clnzbqll <clnzbqll@tiedada.com>
2023-08-23 09:24:33 +0800

    # v-model

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-    <watch/>
+    <vModel/>
 </template>
 
 <script setup>
 import vBind from './components/test/instruct/vBind.vue'
 import vIfElse from './components/test/instruct/vIfElse.vue'
 import vFor from './components/test/instruct/vFor.vue'
+import vModel from './components/test/instruct/vModel.vue'
+
 import methods from './components/test/event/methods.vue'
 import computed from './components/test/event/computed.vue'
 import watch from './components/test/event/watch.vue'
+
 import array from './components/test/array/array.vue'
 </script>
---
+++ src/components/test/instruct/vModel.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- v-model用于form，input，textarea和select元素上创建双向数据绑定 -->
+    <input v-model="msg">
+    <p>{{msg}}</p>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg: "渧"
+        }
+    }
+}
+</script>
